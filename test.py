d56ff570baab0ccc05af46d3088d8ecd08e99adf
Dieglory <aya02245@naver.com>
2020-07-30 15:53:25 +0900

    init

---
+++ test.py
@@ -0,0 +1,37 @@
+# from sklearn.datasets import load_iris
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import classification_report
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# iris = load_iris()
+
+# iris_data = iris.data
+# iris_label = iris.target
+# iris_data_s = iris_data[:,2:4]
+
+# print(iris.keys())
+# print(iris.feature_names)
+# print(iris_data)
+# print(iris_label)
+
+# x_train,x_test,y_train,y_test = train_test_split(iris_data_s,iris_label,test_size=0.3,random_state=25)
+
+# sgd_model = SGDClassifier(alpha=0.001,max_iter=500)
+# sgd_model.fit(x_train, y_train)
+# sgd_model_y_pred = sgd_model.predict(x_test)
+# print(classification_report(y_test,sgd_model_y_pred))
+
+# print(np.shape(iris_data_s))
+
+# # ax = fig.add_subplot(111,projection='3d')
+# plt.scatter(iris_data_s[:,0],iris_data_s[:,1])
+# plt.show()
+import numpy as np
+# T0, T1 = np.meshgrid(np.linspace(-1,3,100),np.linspace(-6,2,100))
+# print(T0)
+# print(T1)
+x = np.linspace(0,1,40)
+X = np.vstack((np.ones(len(x)),x)).T
+print(X)
---
+++ visualization.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def costfunction(X,y,theta):
+    m = np.size(y)
+    h = X @ theta    
+    J = np.sum(np.where(0<h-y+1,h-y+1,0))/m
+    return J
+
+
+def gradient_descent(X,y,theta,alpha = 0.0005,num_iters=1000):
+    m = np.size(y)
+    J_history = np.zeros(num_iters)
+    theta_0_hist, theta_1_hist = [], []
+
+    for i in range(num_iters):
+        #Grad function in vectorized form
+        h = X @ theta
+        theta = theta - alpha * (1/m)* (X.T @ (h-y))
+
+        #Cost and intermediate values for each iteration
+        J_history[i] = costfunction(X,y,theta)
+        theta_0_hist.append(theta[0,0])
+        theta_1_hist.append(theta[1,0])
+
+    return theta,J_history, theta_0_hist, theta_1_hist
+
+x = np.linspace(0,1,40)
+noise = 1*np.random.uniform(size = 40)
+y = np.sin(x * 1.5 * np.pi ) 
+y_noise = (y + noise).reshape(-1,1)
+X = np.vstack((np.ones(len(x)),x)).T
+
+
+#Setup of meshgrid of theta values
+T0, T1 = np.meshgrid(np.linspace(-1,3,100),np.linspace(-6,2,100))
+
+#Computing the cost function for each theta combination
+zs = np.array(  [costfunction(X, y_noise.reshape(-1,1),np.array([t0,t1]).reshape(-1,1)) 
+                     for t0, t1 in zip(np.ravel(T0), np.ravel(T1)) ] )
+#Reshaping the cost values    
+Z = zs.reshape(T0.shape)
+
+
+#Computing the gradient descent
+theta_result,J_history, theta_0, theta_1 = gradient_descent(X,y_noise,np.array([0,-6]).reshape(-1,1),alpha = 0.3,num_iters=1000)
+
+#Angles needed for quiver plot
+anglesx = np.array(theta_0)[1:] - np.array(theta_0)[:-1]
+anglesy = np.array(theta_1)[1:] - np.array(theta_1)[:-1]
+
+fig = plt.figure(figsize = (16,8))
+
+#Surface plot
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+ax.plot_surface(T0, T1, Z, rstride = 5, cstride = 5, cmap = 'jet', alpha=0.5)
+ax.plot(theta_0,theta_1,J_history, marker = '*', color = 'r', alpha = .4, label = 'Gradient descent')
+
+ax.set_xlabel('theta 0')
+ax.set_ylabel('theta 1')
+ax.set_zlabel('Cost function')
+ax.set_title('Gradient descent: Root at {}'.format(theta_result.ravel()))
+ax.view_init(45, 45)
+
+
+#Contour plot
+ax = fig.add_subplot(1, 2, 2)
+ax.contour(T0, T1, Z, 70, cmap = 'jet')
+ax.quiver(theta_0[:-1], theta_1[:-1], anglesx, anglesy, scale_units = 'xy', angles = 'xy', scale = 1, color = 'r', alpha = .9)
+
+plt.show()
