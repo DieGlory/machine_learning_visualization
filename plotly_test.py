0290771d48a4f5be2d526cc846a4cc67e2ff9f25
DieGlory <aya02245@naver.com>
2020-07-31 11:49:05 +0900

    plotly visualization init

--- plotly_test.py
+++ plotly_test.py
@@ -1,77 +1,18 @@
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 import pandas as pd
+import numpy as np
 
-# read in volcano database data
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/volcano_db.csv",
-    encoding="iso-8859-1",
-)
 
-# frequency of Country
-freq = df
-freq = freq.Country.value_counts().reset_index().rename(columns={"index": "x"})
 
-# read in 3d volcano surface data
-df_v = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/volcano.csv")
 
-# Initialize figure with subplots
-fig = make_subplots(
-    rows=2, cols=2,
-    column_widths=[0.6, 0.4],
-    row_heights=[0.4, 0.6],
-    specs=[[{"type": "scattergeo", "rowspan": 2}, {"type": "bar"}],
-           [            None                    , {"type": "surface"}]])
+z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
 
-# Add scattergeo globe map of volcano locations
-fig.add_trace(
-    go.Scattergeo(lat=df["Latitude"],
-                  lon=df["Longitude"],
-                  mode="markers",
-                  hoverinfo="text",
-                  showlegend=False,
-                  marker=dict(color="crimson", size=4, opacity=0.8)),
-    row=1, col=1
-)
 
-# Add locations bar chart
-fig.add_trace(
-    go.Bar(x=freq["x"][0:10],y=freq["Country"][0:10], marker=dict(color="crimson"), showlegend=False),
-    row=1, col=2
-)
+fig = go.Figure(data=[go.Surface(z=z_data.values)])
 
-# Add 3d surface of volcano
-fig.add_trace(
-    go.Surface(z=df_v.values.tolist(), showscale=False),
-    row=2, col=2
-)
-
-# Update geo subplot properties
-fig.update_geos(
-    projection_type="orthographic",
-    landcolor="white",
-    oceancolor="MidnightBlue",
-    showocean=True,
-    lakecolor="LightBlue"
-)
-
-# Rotate x-axis labels
-fig.update_xaxes(tickangle=45)
-
-# Set theme, margin, and annotation in layout
-fig.update_layout(
-    template="plotly_dark",
-    margin=dict(r=10, t=25, b=40, l=60),
-    annotations=[
-        dict(
-            text="Source: NOAA",
-            showarrow=False,
-            xref="paper",
-            yref="paper",
-            x=0,
-            y=0)
-    ]
-)
+fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+                  width=500, height=500,
+                  margin=dict(l=65, r=50, b=65, t=90))
 
 fig.show()
